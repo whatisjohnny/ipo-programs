4e2cf239d22f8b20395b91c55a4fd6ec36bfae2e
whatisjohnny <docklocket@gmail.com>
2022-10-31 11:19:23 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,89 @@
+// Question 1
+
+// Input
+// let dividend = +prompt("Enter a dividend:");
+// let divisor = +prompt("Enter a divisor:");
+
+// Process
+// let remainder = dividend % divisor;
+
+// Output
+// alert(`${dividend} divided by ${divisor} gives a remainder of ${remainder}.`);
+
+// Question 2
+
+// Input
+// let colour = prompt("Enter a Colour");
+// let friendname = prompt("Enter a Name");
+
+// Process
+
+// N/A
+
+// Output
+// alert(`My friend ${friendname} likes ${colour} bananas.`);
+
+// Question 3
+
+// Input
+
+// let colour = prompt("Enter a Colour");
+// let subject = prompt("Enter a Subject");
+// let noun = prompt("Enter a Noun");
+
+// Process
+
+// N/A
+
+// Output
+
+// alert(`In ${subject} class, our teacher was wearing a ${colour} ${noun}.`);
+
+// Question 4
+
+// Input
+
+// let Ftemp = +prompt("Enter temp in degrees Fahrenheit:");
+
+// Process
+
+// let Ctemp = ((Ftemp - 32) * 5) / 9;
+
+// Output
+
+// alert(`${Ftemp} degrees Fahrenheit is ${Ctemp} degrees Celcius`);
+
+// Question 5
+
+// Input
+
+// let billA = +prompt("Enter starting bill amount($):");
+// et tipA = +prompt("Enter percentage to tip(%):");
+
+// Process
+
+// let billF = billA / tipA + billA;
+// let bC = billA / tipA;1
+
+// Output
+
+// alert(`$${billA} bill plus a tip of $${bC} = ${billF}`);
+
+// Question 61
+
+// distance = Math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
+
+// Input
+
+// let x1 = +prompt("Enter Coordinate of X1");
+// let y1 = +prompt("Enter Coordinate of Y1");
+// let x2 = +prompt("Enter Coordinate of X2");
+// let y2 = +prompt("Enter Coordinate of Y2");
+
+// Process
+
+// let dstnc = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+// Output
+
+// alert(`Distance between the two points is ${dstnc}`);
